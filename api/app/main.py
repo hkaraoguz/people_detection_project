23d67b23e774a642dae27c3baba4a4a2848e54fd
hkaraoguz <hkaraoguz@yahoo.com.tr>
2021-06-13 23:32:15 +0200

    initial commit

---
+++ api/app/main.py
@@ -0,0 +1,131 @@
+from typing import Optional
+
+import os
+
+import datetime
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel, BaseSettings
+
+from pymongo import MongoClient, database, collection, cursor, settings
+from bson.objectid import ObjectId
+import base64
+
+import numpy as np
+
+import cv2
+import gridfs
+
+
+class Settings(BaseSettings):
+    mongo_host: str = '127.0.0.1'
+    mongo_port: int = 27017
+    
+    class Config:
+        env_file = ".env"
+
+
+class TimeFilter(BaseModel):
+    lower: float
+    upper: float
+
+class Item(BaseModel):
+    tracking_id: int
+    first_timestamp: float
+    framepath: str
+
+settings = Settings()
+mongo_client = None
+app = FastAPI()
+db = None
+fs = None
+
+origins = [
+    "http://localhost",
+    "http://127.0.0.1"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.on_event("startup")
+def startup_event():
+
+    global db
+    global fs
+
+    mongo_client =  MongoClient(settings.mongo_host,settings.mongo_port,connect=True)
+
+    db = mongo_client.get_database('people_project')
+
+    fs = gridfs.GridFS(db)
+
+    print(db.list_collection_names())
+
+   #print(db['people'].insert_one({'person':12}))
+
+
+
+
+@app.get("/")
+async def root():
+
+    records = db['person'].find(limit=0)
+    return_dict = []
+    for item in records:
+         # get the image from gridfs
+        gOut = fs.get(ObjectId(item['image_id']))
+
+        # get the image from gridfs
+        gOut = fs.get(ObjectId(item['image_id']))
+
+        # convert bytes to ndarray
+        img = np.frombuffer(gOut.read(), dtype=np.uint8)
+
+        img = np.reshape(img, np.array([ 480, 704, 3 ]))
+
+        retval, buffer = cv2.imencode('.jpg', img)
+        jpg_as_text = base64.b64encode(buffer)
+
+        time = datetime.datetime.fromtimestamp(int(item['first_timestamp'])).strftime(format="%Y-%m-%d %H:%M:%S")
+        return_dict.append({'tracking_id':item['tracking_id'],'timestamp':time,'image':jpg_as_text})
+    return return_dict#{"message": "Hello World"}
+
+@app.post("/filtertime/")
+async def filtertime(timefilter: TimeFilter):
+    print(timefilter.lower/1000)
+    print(timefilter.upper/1000)
+    records = db['person'].find({"first_timestamp":{"$gte":timefilter.lower/1000, "$lt":timefilter.upper/1000}},limit=0)
+    print(records.count())
+    #print(records)
+    return_dict = []
+    for item in records:
+        # get the image from gridfs
+        gOut = fs.get(ObjectId(item['image_id']))
+
+        # convert bytes to ndarray
+        img = np.frombuffer(gOut.read(), dtype=np.uint8)
+
+        img = np.reshape(img, np.array([ 480, 704, 3 ]))
+
+        retval, buffer = cv2.imencode('.jpg', img)
+        jpg_as_text = base64.b64encode(buffer)
+
+        #jpg_as_text = base64.b64encode(img)
+
+        # reshape to match the image size
+        #
+        time = datetime.datetime.fromtimestamp(int(item['first_timestamp'])).strftime(format="%Y-%m-%d %H:%M:%S")
+        return_dict.append({'tracking_id':item['tracking_id'],'timestamp':time,'image':jpg_as_text})
+    return return_dict#{"message": "Hello World"}
+
+
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
---
+++ object_detection/main.py
@@ -0,0 +1,336 @@
+import os
+import time
+import sys
+import torchvision
+import numpy as np
+from pymongo import MongoClient, database
+import gridfs
+import torch
+import datetime
+
+
+sys.path.append('../sort')
+print(sys.path)
+from sort import *
+#import torch
+#import numpy as np
+#from torchvision import transforms
+
+from cv2 import cv2
+
+class Person(object):
+
+    def __init__(self, tracking_id):
+        self.frames  = []
+        self.tracking_id = tracking_id
+        self.boxes = []
+        self.first_timestamp = None
+
+
+
+# torchvision.models.detection.fasterrcnn_mobilenet_v3_large_320_fpn(pretrained=True)
+# torchvision.models.detection.retinanet_resnet50_fpn(pretrained=True)
+#model = torchvision.models.detection.fasterrcnn_mobilenet_v3_large_fpn(pretrained=True, min_size=400, max_size=600)
+model = torchvision.models.detection.fasterrcnn_mobilenet_v3_large_fpn(pretrained=True)
+
+
+dev = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+#dev = torch.device("cpu")
+model.to(dev)
+model.eval()
+
+
+
+COCO_INSTANCE_CATEGORY_NAMES = ['__background__', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
+'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'N/A', 'stop sign',
+'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow',
+'elephant', 'bear', 'zebra', 'giraffe', 'N/A', 'backpack', 'umbrella', 'N/A', 'N/A',
+'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
+'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket',
+'bottle', 'N/A', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl',
+'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
+'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'N/A', 'dining table',
+'N/A', 'N/A', 'toilet', 'N/A', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone',
+'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'N/A', 'book',
+'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush'
+]
+
+print(len(COCO_INSTANCE_CATEGORY_NAMES))
+
+url = 'http://73.13.148.126:8082/mjpg/video.mjpg'#'http://72.43.190.171:81/mjpg/video.mjpg'
+
+#'http://184.153.62.129:82/mjpg/video.mjpg' #'http://189.174.160.218:6001/mjpg/video.mjpg'
+#'http://180.44.188.214:3000/mjpg/video.mjpg'#'http://67.250.253.16:8081/mjpg/video.mjpg' #'http://82.58.15.68:83/mjpg/video.mjpg' #http://122.58.10.67:83/mjpg/video.mjpg
+
+
+
+color = (255, 0, 0)
+thickness = 2
+
+frames = []
+start_time = None
+end_time = None
+last_time = None
+person_counter = 0
+frame_counter = 0
+person_boxes = []
+
+MAX_TIME = 10
+
+curr_path = os.path.dirname(os.path.abspath(__file__))
+
+pictures_path = os.path.join(curr_path,'pictures')
+
+if not os.path.exists(pictures_path):
+    os.mkdir(pictures_path)
+
+def initialize_mongo_client():
+
+    try:
+        mongo_client =  MongoClient('127.0.0.1',27017,connect=True)
+        db = mongo_client.get_database('people_project')
+        fs = gridfs.GridFS(db)
+
+    except Exception as ex:
+        print(str(ex))
+
+        return None
+
+    return db,fs
+
+
+    #print(db.list_collection_names())
+
+def visualize_detections(input_frame, dets):
+
+    for box in dets:
+        start_point = (int(box[0]), int(box[1]))
+        end_point = (int(box[2]), int(box[3]))
+        input_frame = cv2.rectangle(input_frame, start_point, end_point, color, thickness)
+
+    return input_frame
+
+
+def get_people_tracks(tracker, dets):
+
+    trackers = tracker.update(dets)
+
+    return trackers
+    #print(trackers)
+
+    for track in trackers:
+        print('track: ',track)
+        #print(track)
+
+
+def get_detections(frame, model):
+
+    process_frame = frame
+
+    process_frame = cv2.cvtColor(process_frame, cv2.COLOR_BGR2RGB )
+
+    # Transform the image to tensor
+    tran = torchvision.transforms.ToTensor()
+
+
+    # Process the image
+    tens = tran(process_frame)
+
+    tens = tens.to(dev)
+
+    try:
+        predictions = model([tens])
+
+        #predictions = predictions.cpu()
+
+        if predictions:
+
+            boxes = predictions[0]['boxes'].cpu().detach().numpy()
+            scores = predictions[0]['scores'].cpu().detach().numpy()
+            label_indexes = predictions[0]['labels'].cpu().detach().numpy()
+
+            return boxes, scores, label_indexes
+
+        return np.zeros(1), None, None
+
+    except Exception as ex:
+        print(ex)
+        return np.zeros(1), None, None
+
+def track_people(trackers, frame):
+    ids = []
+    #print(type(trackers))
+    if 'numpy' in str(type(trackers)):
+        #print(trackers)
+        if trackers.size > 0:
+            ids = trackers[:,4]
+            ids = ids.tolist()
+            
+
+        # get unique ids
+        ids = list(set(ids))
+
+    for person_id in ids:
+        float_id = person_id
+        person_id = int(person_id)
+        if not people_track_start_dict.get(person_id):
+            
+            indices = np.where(trackers[:,4] == float_id)[0]
+            person = Person(person_id)
+            person_box = None
+            if indices.size >0:
+                person_box = trackers[indices[-1],:4].astype(int).tolist()
+                person.boxes.append(person_box)
+                #print(person_box)
+            if person_box:
+                new_frame = visualize_detections(frame.copy(),[person_box])
+                people_track_start_dict[person_id] = time.time()
+                person.frames.append(new_frame)
+                person.first_timestamp = time.time()
+                people_dict[person_id] = person
+        else:
+            person = people_dict[person_id]
+            #person.frames.append(frame)
+            indices = np.where(trackers[:,4] == float_id)[0]
+            person_box = None
+
+            if indices.size >0:
+                person_box = trackers[indices[-1],:4].astype(int).tolist()
+                person.boxes.append(person_box)
+                new_frame = visualize_detections(frame.copy(),[person_box])
+                person.frames.append(new_frame)
+            
+            people_dict[person_id] = person
+
+    del_indexes = []
+    for key, value in people_track_start_dict.items():
+        if time.time() - value >= MAX_TIME:
+                person = people_dict[key]
+
+                if len(person.frames) >= 3:
+                    print(person.first_timestamp)
+                    print(person.tracking_id)
+
+                    # convert ndarray to string
+                    imageString = person.frames[0].tobytes()
+
+                    # store the image
+                    imageID = grid_fs.put(imageString, encoding='utf-8')
+                    
+
+                    '''
+                    folder_path = os.path.join(pictures_path, datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S'),str(person.tracking_id))
+                    if not os.path.exists(folder_path):
+                        os.makedirs(folder_path)
+                    for i in range(3):
+                        filename = str(i)+'.jpg'
+                        filename=os.path.join(folder_path,filename)
+                        cv2.imwrite(filename,person.frames[i])
+                    '''
+                    mongo_dict =  vars(person)
+
+                    mongo_dict['image_id'] = imageID
+                    mongo_dict['image_shape'] = person.frames[0].shape
+                    
+                    del mongo_dict['frames']
+                    del mongo_dict['boxes']
+                    
+                    #mongo_dict['framepath'] = folder_path
+                    mongo_db['person'].insert_one(mongo_dict)
+                del_indexes.append(key)
+
+    for index in del_indexes:
+        del people_dict[index]
+        del people_track_start_dict[index]
+
+
+
+#create instance of the SORT tracker
+mot_tracker = Sort(max_age=5,
+                       min_hits=2,iou_threshold=0.2)
+
+people_track_start_dict = dict()
+
+people_dict = dict()
+
+mongo_db,grid_fs  = initialize_mongo_client()
+
+
+if mongo_db == None:
+    sys.exit(-1)
+
+
+
+stream = cv2.VideoCapture(url)
+
+# infinite loop
+while True:
+
+    # read frames
+    (grabbed, frame) = stream.read()
+
+    # check if frame empty
+    if not grabbed:
+        print('opencv is buggy')
+        continue
+        #break
+
+    boxes, scores, label_indexes = get_detections(frame,model)
+
+    person_boxes = []
+    trackers = []
+
+    if np.all(boxes) == 0:
+        get_people_tracks(mot_tracker, np.empty((0,5)))
+        track_people(trackers,frame.copy())
+        continue
+
+    #print(predictions[0]['boxes'].detach().numpy())
+
+    for index, box in enumerate(boxes):
+
+        score = scores[index]
+        label_index = label_indexes[index]
+        label = COCO_INSTANCE_CATEGORY_NAMES[label_index]
+
+        if score > 0.8 and label=='person':
+
+            person_counter += 1
+
+            new_box = box.astype(int)
+
+            new_box = new_box.tolist()
+
+            new_box.append(score)
+
+            person_boxes.append(new_box)
+
+            #print('score: ',score,' label: ',label)
+            #break
+    if person_boxes:
+        #frame = visualize_detections(frame,person_boxes)
+        #print(person_boxes)
+        trackers = get_people_tracks(mot_tracker, np.asarray(person_boxes))
+
+        '''
+        if frames:
+            if(time.time()-start_time > MAX_TIME):
+                print(float(person_counter)/frame_counter)
+                print(len(frames))
+                frames = []
+                frame_counter = 0
+                person_counter = 0
+        '''
+    track_people(trackers,frame.copy())
+    frame = visualize_detections(frame,person_boxes)
+    # Show output window
+    cv2.imshow("Output Frame", frame)
+
+    key = cv2.waitKey(1) & 0xFF
+    # check for 'q' key-press
+    if key == ord("q"):
+        #if 'q' key-pressed break out
+        break
+
+cv2.destroyAllWindows()
+# close output window
